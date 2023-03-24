d35cb78bd43540af59f7956621b0e897e5940850
brijk20 <brijeshv72@gmail.com>
2023-03-24 01:06:15 -0400

    added ipfs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// import { useState } from "react";
+
 import AccountData from "./components/AccountSection/AccountData";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu/Menu";
@@ -8,6 +10,8 @@ import { Task } from "./interfaces";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { modalActions } from "./store/Modal.store";
 import { tasksActions } from "./store/Tasks.store";
+// import { addFile, getFile } from "./ipfs";
+
 
 const App: React.FC = () => {
   const modal = useAppSelector((state) => state.modal);
@@ -21,6 +25,22 @@ const App: React.FC = () => {
   const createNewTaskHandler = (task: Task) => {
     dispatch(tasksActions.addNewTask(task));
   };
+
+  //
+  // const [cid, setCid] = useState("");
+  // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const file = event.target.files[0];
+  //   addFile(file).then((cid) => {
+  //     setCid(cid);ia
+  //   });
+  // };
+
+  // const handleGetFile = async () => {
+  //   const content = await getFile(cid);
+  //   console.log(content);
+  // };
+
+
 
   return (
     <div className="bg-slate-200 min-h-screen text-slate-600 dark:bg-slate-900 dark:text-slate-400 xl:text-base sm:text-sm text-xs">
